c76b5278b57db4d481a42dc492d446d4aa7f7ee0
thomas bartier <thomas.bartier@gmail.com>
2025-02-07 14:39:51 +0100

    new app illustration

--- src/components/aboutME/AboutMe.jsx
+++ src/components/aboutME/AboutMe.jsx
@@ -5,6 +5,13 @@ const AboutMe = () => {
   return (
     <div className='flex flex-col'>
       <p>AboutMe</p>
+      
+      <div className='flex gap-5'>
+      <img src="/src/assets/illustrations/dev1.svg" alt="illustration Développeur by UnDraw" className='h-60 w-60'/>
+      <span>illustration Développeur by UnDraw</span>
+      <img src="/src/assets/illustrations/dev2.svg" alt="illustration Développeur by Storyset" className='h-60 w-60'/>
+      <span>illustration Développeur by Storyset</span>
+      </div>
       <LinksAbout />
     </div>
   )
