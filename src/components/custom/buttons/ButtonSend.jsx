c9e5e375131db8433a7059cbcfcebc7e6f190d07
thomas bartier <thomas.bartier@gmail.com>
2025-03-03 16:06:56 +0100

    fix color send button

--- src/components/custom/buttons/ButtonSend.jsx
+++ src/components/custom/buttons/ButtonSend.jsx
@@ -10,10 +10,10 @@ const ButtonSend = ({ loading }) => {
     <div className="flex items-center justify-center gap-4 max-sm:gap-1 max-sm:mr-1 max-sm:text-xs">
       <button
         type="submit"
-        className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-teal-600 transition duration-300 ease-out border-2 border-teal-600 rounded-2xl shadow-md group"
+        className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-[#0891b2] transition duration-300 ease-out border-2 border-[#0891b2] rounded-2xl shadow-md group"
         disabled={!loading ? false : true}
       >
-        <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-teal-600 group-hover:translate-x-0 ease">
+        <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-[#0891b2] group-hover:translate-x-0 ease">
         {!loading ? 
                   <svg
             className="w-6 h-6"
@@ -32,7 +32,7 @@ const ButtonSend = ({ loading }) => {
           :
           <Ellipsis />}
         </span>
-        <span className="absolute flex items-center justify-center w-full h-full dark:text-white text-teal-600 transition-all duration-300 transform group-hover:translate-x-full ease">
+        <span className="absolute flex items-center justify-center w-full h-full dark:text-white text-[#0891b2] transition-all duration-300 transform group-hover:translate-x-full ease">
           {!loading ? t("CONTACT.SEND") : t("CONTACT.SENDING")}
         </span>
         <span className="relative invisible">Button Text</span>
