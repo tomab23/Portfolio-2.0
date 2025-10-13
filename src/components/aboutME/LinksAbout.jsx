75dd66cb2301cdc432c4ef81f3a6785cd25f72eb
thomas bartier <thomas.bartier@gmail.com>
2025-10-13 13:51:01 +0200

    update projects & update linkedin link

--- src/components/aboutME/LinksAbout.jsx
+++ src/components/aboutME/LinksAbout.jsx
@@ -7,7 +7,7 @@ const LinksAbout = () => {
       {/* CV */}
       <CustomLinkButton title={"CV"} link={false} page={"cv"} classname={"bg-gray-500 dark:bg-gray-500 w-[4.5rem]"} />
       {/* Linkedin */}
-      <CustomLinkButton title={"LinkedIn"} link={true} page={"https://www.linkedin.com/in/thomas-bartier-185752138/"} classname={"bg-[#0e76a8] dark:bg-[#0e76a8] "} />
+      <CustomLinkButton title={"LinkedIn"} link={true} page={"https://www.linkedin.com/in/thomas-bartier/"} classname={"bg-[#0e76a8] dark:bg-[#0e76a8] "} />
       {/* Github */}
       <CustomLinkButton title={"GitHub"} link={true} page={"https://github.com/tomab23"} classname={"bg-[#171515] dark:bg-[#171515]"} />
       {/* Mes Projets */}
